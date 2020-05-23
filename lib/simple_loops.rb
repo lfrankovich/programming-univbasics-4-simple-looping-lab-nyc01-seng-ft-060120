c4edce3e4486e5709319261d2604c650adec13c3
Lauren Frankovich <frankovister@gmail.com>
2020-05-23 20:28:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,42 @@
 # Write your methods here
+
+#def method_name(parameter)
+#  count = 0
+#  while count < (something)
+#    # do something with the parameter
+#    count += 1
+#  end
+#end
+
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+def loop_message_n_times(message, number)
+  count = 0
+  while count < number do
+    puts message
+    count += 1
+  end
+end
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array
+    count += 1
+  end
+end
+  
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    puts array
+    count += 1
+  end
+end
+  
