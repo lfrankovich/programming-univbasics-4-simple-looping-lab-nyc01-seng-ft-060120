e8f1ff2c016bfcb77847228b778865ce49239df5
Lauren Frankovich <frankovister@gmail.com>
2020-05-23 20:30:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,13 +1,3 @@
-# Write your methods here
-
-#def method_name(parameter)
-#  count = 0
-#  while count < (something)
-#    # do something with the parameter
-#    count += 1
-#  end
-#end
-
 def loop_message_five_times(message)
   count = 0
   while count < 5 do
